d6b6a9eb2f85cb8908d017c3843c567179b88001
Noah Sternberg <nsternberg@murraystate.edu>
2024-01-20 21:59:22 -0600

    Create factories and controllers

---
+++ api/src/main/java/com/dd/api/restapi/controllers/OffensivePlayerController.java
@@ -0,0 +1,9 @@
+package com.dd.api.restapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/diamond-data/api/offensive-players")
+public class OffensivePlayerController {
+}
