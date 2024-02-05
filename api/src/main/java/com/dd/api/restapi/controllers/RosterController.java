d4556e8dd4583d94238bb67534d8f33f4fb84b37
Noah Sternberg <noahcs2002@gmail.com>
2024-02-05 14:18:17 -0600

    begin roster work

--- api/src/main/java/com/dd/api/restapi/controllers/RosterController.java
+++ api/src/main/java/com/dd/api/restapi/controllers/RosterController.java
@@ -1,8 +1,14 @@
 package com.dd.api.restapi.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dd.api.database.Context;
+import com.dd.api.restapi.factories.RosterFactory;
+import com.dd.api.restapi.models.Roster;
+import com.dd.api.restapi.requestmodels.CreateRosterRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
 
 @RestController
 @RequestMapping("/diamond-data/api/rosters")
@@ -12,6 +18,18 @@ public class RosterController {
     @RequestMapping("xyz")
     public String connect() {
 	return "RosterController Online";
+    }
+    
+    @PostMapping
+    public Roster createRoster(@RequestBody CreateRosterRequest rosterRequest) {
+	Context context = new Context();
+	try(Connection connection = DriverManager.getConnection(context.getConnectionString(), context.getUsername(), context.getPassword())) {
+	    return RosterFactory.createRoster(rosterRequest, connection);
+	}
+	catch (Exception ex) {
+	    System.out.println(ex);
+	    return null;
+	}
     }
     
 }
--- api/src/main/java/com/dd/api/restapi/factories/RosterFactory.java
+++ api/src/main/java/com/dd/api/restapi/factories/RosterFactory.java
@@ -1,4 +1,20 @@
 package com.dd.api.restapi.factories;
 
+import com.dd.api.restapi.models.Roster;
+import com.dd.api.restapi.requestmodels.CreateRosterRequest;
+
+import java.sql.Connection;
+
 public class RosterFactory {
+    public static Roster createRoster(CreateRosterRequest rosterRequest, Connection connection) {
+	
+	String sql = """
+	    INSERT INTO sp24.dd_rosters
+	    (id, managerId, expiry, catcher, firstBase,
+	    secondBase, thirdBase, shortStop, leftField,
+	    rightField, centerField, startingPitcher, ghosted_date) VALUES
+	    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+	""";
+	
+    }
 }
