72dc8321f6c591d42c93db0fb51967da7099d83d
Jaylee Baker <jbaker50@murraystate.edu>
2024-04-04 11:42:33 -0500

    Home page changes

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -53,47 +53,47 @@ useEffect(() => {
   const upcomingDays = getUpcomingDays();
 
   return (
-    <div className="home" style={{ backgroundImage: 'url(${})'}}>
-        <Navbar/>
-      <div className="headerContainer">
-        <h1>Welcome {selectedTeam}</h1>
-        <div className="teamDropdown">
-          <select value={selectedTeam} onChange={(e) => handleTeamChange(e.target.value)}>
-            {teams.map((team, index) => (
-              <option key={index} value={team}>{team}</option>
-            ))}
-          </select>
-        </div>
-        <div className="buttonsContainer">
-          <Link to="/PlayerManagement" className="button">Player Management</Link>
-          <Link to="/TeamStats" className="button">Team Stats</Link>
-          <Link to="/UserManagement" className="button">User Management</Link>
-          <Link to="" className="button">Insights</Link>
-          <Link to="" className="button">Bulk Entry</Link>
-        </div>
-        <div className="startingLineup">
-          <h2>Starting Lineup</h2>
-          <div className="lineup">
-            <div className="player">Player 1</div>
-            <div className="player">Player 2</div>
-            <div className="player">Player 3</div>
-            <div className="player">Player 4</div>
-            <div className="player">Player 5</div>
-            <div className="player">Player 6</div>
-            <div className="player">Player 7</div>
-            <div className="player">Player 8</div>
-            <div className="player">Player 9</div>
-            <div className="player"> Starting Pitcher</div>
-          </div>
-          <div className="calendar">
-            <h2>Upcoming Games</h2>
-            <div className="daysContainer">
-              {upcomingDays.map((day, index) => (
-                <div key={index} className="day">{day}</div>
+    <div>
+      <Navbar/>
+      <div className="home">
+          <h1 className='title'>Welcome {selectedTeam}</h1>
+          <div className="teamDropdown">
+            <select value={selectedTeam} onChange={(e) => handleTeamChange(e.target.value)}>
+              {teams.map((team, index) => (
+                <option key={index} value={team}>{team}</option>
               ))}
+            </select>
+          </div>
+          <div className="buttonsContainer">
+            <Link to="/PlayerManagement" className="button">Player Management</Link>
+            <Link to="/TeamStats" className="button">Team Stats</Link>
+            <Link to="/UserManagement" className="button">User Management</Link>
+            <Link to="" className="button">Insights</Link>
+            <Link to="" className="button">Bulk Entry</Link>
+          </div>
+          <div className="startingLineup">
+            <h2>Starting Lineup</h2>
+            <div className="lineup">
+              <div className="player">Player 1</div>
+              <div className="player">Player 2</div>
+              <div className="player">Player 3</div>
+              <div className="player">Player 4</div>
+              <div className="player">Player 5</div>
+              <div className="player">Player 6</div>
+              <div className="player">Player 7</div>
+              <div className="player">Player 8</div>
+              <div className="player">Player 9</div>
+              <div className="player"> Starting Pitcher</div>
+            </div>
+            <div className="calendar">
+              <h2>Upcoming Games</h2>
+              <div className="daysContainer">
+                {upcomingDays.map((day, index) => (
+                  <div key={index} className="day">{day}</div>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
       </div>
     </div>
   );
