308cf576004cb835697234f2d7c767d09f918937
Jaylee Baker <jbaker50@murraystate.edu>
2024-04-12 22:17:01 -0500

    Create logout confirmation

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import Diamond_Data_Logo_White from "../assets/Diamond_Data_Logo_White.PNG";
 import Diamond_Data_Transparent from "../assets/Diamond_Data_Transparent.png"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
 import SettingsIcon from '@mui/icons-material/Settings'; // Import the Settings icon
 import '../styles/Navbar.scss';
 
 function Navbar() {
     const [openLinks, setOpenLinks] = useState(false);
+    const navigate = useNavigate();
 
     const toggleNavbar = () => {
         setOpenLinks(!openLinks);
-    }
+    };
+
+    const handleLogout = () => {
+        if (window.confirm("Logout?")) {
+            navigate('/');
+        }
+    };
 
     return (
         <div className="navbar">
@@ -19,34 +26,34 @@ function Navbar() {
                 <img src={Diamond_Data_Transparent} />
                 <h4 style={{color:'white'}}> {JSON.parse(localStorage.getItem('sessionData')).name} ({JSON.parse(localStorage.getItem('sessionData')).email}) </h4>
                 <div className="hiddenLinks">
-                    <Link to="/Home"> Home </Link>
+                    <Link className="navbarLink" to="/Home"> Home </Link>
                     <Link to="/PlayerManagement"> Player Management </Link>
                     <Link to="/TeamStats"> Team Stats </Link>
                     <Link to="/PlayerStats"> Player Stats </Link>
                     <Link to="/Roster"> Roster </Link>
                     {/* <Link to="/Insights"> Insights </Link> */}
                     <Link to="/BulkEntry"> Bulk Entry </Link>
-                    <Link to="/"> Logout </Link>
+                    <button className="navbarLink" onClick={handleLogout}>Logout</button>
                     <Link to="/Settings">
                         <SettingsIcon style={{ fontSize: 32, color: '#FFF' }} />
                     </Link>
                 </div>
             </div>
             <div className="rightSide">
-                <Link to="/Home"> Home </Link>
-                <Link to="/PlayerManagement"> Player Management </Link>
-                <Link to="/TeamStats"> Team Stats </Link>
-                <Link to="/PlayerStats"> Player Stats </Link>
-                <Link to="/Roster"> Roster </Link>
+                <Link className="navbarLink" to="/Home"> Home </Link>
+                <Link className="navbarLink" to="/PlayerManagement"> Player Management </Link>
+                <Link className="navbarLink" to="/TeamStats"> Team Stats </Link>
+                <Link className="navbarLink" to="/PlayerStats"> Player Stats </Link>
+                <Link className="navbarLink" to="/Roster"> Roster </Link>
                 {/* <Link to="/Insights"> Insights </Link> */}
-                <Link to="/BulkEntry"> Bulk Entry </Link>
-                <Link to="/"> Logout </Link>
-                <Link to="/Settings">
+                <Link className="navbarLink" to="/BulkEntry"> Bulk Entry </Link>
+                <button className="navbarLink" onClick={handleLogout}>Logout</button>
+                <Link className="navbarLink" to="/Settings">
                     <SettingsIcon style={{ fontSize: 32, color: '#FFF' }} />
                 </Link>
-                <button onClick={toggleNavbar}>
+                {/* <button onClick={toggleNavbar}>
                     <ReorderIcon />
-                </button>
+                </button> */}
             </div>
         </div>
     )
